6e10cf10013a74306fef6c5ab960f41450e07e84
Alex <dev@alexnguyen.co.nz>
2023-12-13 13:08:19 +1300

    image testing - updated config for trailing slashes - updated header component

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -6,6 +6,7 @@ import awsAmplify from 'astro-aws-amplify';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+	trailingSlash: "always",
 	integrations: [mdx(), sitemap()],
 	output: 'server',
 	adapter: awsAmplify(),
